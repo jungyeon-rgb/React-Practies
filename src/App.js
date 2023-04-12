eca8dca9d98ba3ef5fadd0886e096edf85b36bb3
김정연 <jy.keem2468@gmail.com>
2023-04-12 16:16:11 +0900

    :zap: ExpensesList 파일 생성을 통해 Expenses.js의 코드 개선

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", 
-    title: "New TV", 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12) 
-  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
     title: "Car Insurance",
@@ -32,8 +28,8 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {
-    setExpenses(prevExpenses => {
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
@@ -44,6 +40,6 @@ const App = () => {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
-import Card from '../UI/Card';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Updated!');
-    };
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
 
-    return (
-    <Card className="expense-item">
+  return (
+    <li>
+      <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
-    </Card>
-    );
-}
+      </Card>
+    </li>
+  );
+};
 
 export default ExpenseItem;
